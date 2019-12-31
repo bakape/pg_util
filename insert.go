d0311dc101ad277d34643aeb9c512b6a856d4a52
bakape <bakape@gmail.com>
2019-12-31 21:37:58 +0200

    Insert statement builder

---
+++ insert.go
@@ -0,0 +1,176 @@
+package pg_util
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+	"sync"
+)
+
+var (
+	insertCache sync.Map
+	argPool     = sync.Pool{
+		New: func() interface{} {
+			return []interface{}(nil)
+		},
+	}
+	dedupMapPool = sync.Pool{
+		New: func() interface{} {
+			return make(map[string]struct{})
+		},
+	}
+)
+
+// Options for building insert statement
+type InsertOpts struct {
+	// Table to insert into
+	Table string
+
+	// Struct that will have all its public fields written to the database.
+	//
+	// Use `db:"name"` to override the default name of a column.
+	// Fields with a `db:"-"` tag will be skipped
+	//
+	// First the fields in struct itself are scanned and then the fields in any
+	// embedded structs using depth first search.
+	// If duplicate column names (from the struct field name or `db` struct tag)
+	// exist, the first found value will ber used.
+	Data interface{}
+
+	// Optional prefix to statement
+	Prefix string
+
+	// Optional suffix to statement
+	Suffix string
+}
+
+// Build and cache insert statement for all fields of data. This includes
+// embedded struct fields.
+//
+// See InsertOpts for further documentation.
+func BuildInsert(o InsertOpts) (sql string, args []interface{}) {
+	rootT := reflect.TypeOf(o.Data)
+	k := struct {
+		table, prefix, suffix string
+		typ                   reflect.Type
+	}{
+		table:  o.Table,
+		prefix: o.Prefix,
+		suffix: o.Suffix,
+		typ:    rootT,
+	}
+	_sql, cached := insertCache.Load(k)
+	if cached {
+		sql = _sql.(string)
+	}
+
+	args = argPool.Get().([]interface{})
+	var (
+		w          strings.Builder
+		scanStruct func(parentV reflect.Value, parentT reflect.Type)
+		dedupMap   = dedupMapPool.Get().(map[string]struct{})
+	)
+	defer func() {
+		for k := range dedupMap {
+			delete(dedupMap, k)
+		}
+		dedupMapPool.Put(dedupMap)
+	}()
+	scanStruct = func(parentV reflect.Value, parentT reflect.Type) {
+		type desc struct {
+			reflect.Value
+			reflect.Type
+		}
+
+		var (
+			embedded []desc
+			l        = parentT.NumField()
+		)
+		for i := 0; i < l; i++ {
+			var (
+				f    = parentT.Field(i)
+				name string
+				tag  = f.Tag.Get("db")
+			)
+			switch tag {
+			case "-":
+				continue
+			case "":
+				name = f.Name
+			default:
+				name = tag
+			}
+
+			v := parentV.Field(i)
+			if f.Anonymous {
+				embedded = append(embedded, desc{
+					v,
+					f.Type,
+				})
+				continue
+			}
+
+			if _, ok := dedupMap[name]; ok {
+				continue
+			}
+
+			if !cached {
+				if len(dedupMap) != 0 {
+					w.WriteByte(',')
+				}
+				w.WriteString(name)
+			}
+			dedupMap[name] = struct{}{}
+			args = append(args, v.Interface())
+		}
+
+		for _, d := range embedded {
+			scanStruct(d.Value, d.Type)
+		}
+	}
+
+	if !cached {
+		if o.Prefix != "" {
+			w.WriteString(o.Prefix)
+			w.WriteByte(' ')
+		}
+		fmt.Fprintf(&w, "insert into %s (", o.Table)
+	}
+
+	scanStruct(reflect.ValueOf(o.Data), rootT)
+
+	if !cached {
+		w.WriteString(") values (")
+		for i := 0; i < len(dedupMap); i++ {
+			if i != 0 {
+				w.WriteByte(',')
+			}
+			w.WriteByte('$')
+			if i < 9 {
+				w.WriteByte(byte(i) + '0' + 1) // Avoids allocation
+			} else {
+				w.Write(strconv.AppendUint(nil, uint64(i+1), 10))
+			}
+		}
+		w.WriteByte(')')
+
+		if o.Suffix != "" {
+			w.WriteByte(' ')
+			w.WriteString(o.Suffix)
+		}
+
+		sql = w.String()
+		insertCache.Store(k, sql)
+	}
+
+	return
+}
+
+// Put argument slice back into the pool for reuse. args must not be used after
+// this.
+func ResuseArgs(args []interface{}) {
+	if len(args) != 0 {
+		argPool.Put(args[:0])
+	}
+}
---
+++ insert_test.go
@@ -0,0 +1,172 @@
+package pg_util
+
+import "testing"
+
+func TestTestBuildInsert(t *testing.T) {
+	t.Parallel()
+
+	type testCase struct {
+		name, sql     string
+		opts          InsertOpts
+		args          []interface{}
+		before, after func()
+	}
+
+	type inner struct {
+		F3 int
+	}
+
+	type innerOverlapping struct {
+		F2 int
+	}
+
+	ch := make(chan struct{})
+
+	cases := [...]testCase{
+		{
+			name: "simple",
+			opts: InsertOpts{
+				Table: "t1",
+				Data: struct {
+					F1 string
+					F2 int
+				}{"aaa", 1},
+			},
+			sql:  `insert into t1 (F1,F2) values ($1,$2)`,
+			args: []interface{}{"aaa", 1},
+			after: func() {
+				close(ch)
+			},
+		},
+		{
+			name: "cached",
+			opts: InsertOpts{
+				Table: "t1",
+				Data: struct {
+					F1 string
+					F2 int
+				}{"aaa", 1},
+			},
+			sql:  `insert into t1 (F1,F2) values ($1,$2)`,
+			args: []interface{}{"aaa", 1},
+			before: func() {
+				// Ensure this test always runs after "simple"
+				<-ch
+			},
+		},
+		{
+			name: "with name tag",
+			opts: InsertOpts{
+				Table: "t1",
+				Data: struct {
+					F1 string `db:"field_1"`
+					F2 int
+				}{"aaa", 1},
+			},
+			sql:  `insert into t1 (field_1,F2) values ($1,$2)`,
+			args: []interface{}{"aaa", 1},
+		},
+		{
+			name: "with skipped field",
+			opts: InsertOpts{
+				Table: "t1",
+				Data: struct {
+					F1 string
+					F2 int
+					F3 int `db:"-"`
+				}{"aaa", 1, 1},
+			},
+			sql:  `insert into t1 (F1,F2) values ($1,$2)`,
+			args: []interface{}{"aaa", 1},
+		},
+		{
+			name: "with prefix and suffix",
+			opts: InsertOpts{
+				Table: "t1",
+				Data: struct {
+					F1 string
+					F2 int
+				}{"aaa", 1},
+				Prefix: "with v as (select 1)",
+				Suffix: "returning f1",
+			},
+			sql: `with v as (select 1) insert into t1 (F1,F2) values ($1,$2)` +
+				` returning f1`,
+			args: []interface{}{"aaa", 1},
+		},
+		{
+			name: "with embedded struct",
+			opts: InsertOpts{
+				Table: "t1",
+				Data: struct {
+					F1 string
+					F2 int
+					inner
+				}{"aaa", 1, inner{3}},
+			},
+			sql:  `insert into t1 (F1,F2,F3) values ($1,$2,$3)`,
+			args: []interface{}{"aaa", 1, 3},
+		},
+		{
+			name: "with embedded struct override",
+			opts: InsertOpts{
+				Table: "t2",
+				Data: struct {
+					innerOverlapping
+					F1 string
+					F2 int
+				}{innerOverlapping{3}, "aaa", 1},
+			},
+			sql:  `insert into t2 (F1,F2) values ($1,$2)`,
+			args: []interface{}{"aaa", 1},
+		},
+		{
+			name: "with many args",
+			opts: InsertOpts{
+				Table: "t1",
+				Data: struct {
+					F1 string
+					F2 int
+					F3,
+					F4,
+					F5,
+					F6,
+					F7,
+					F8,
+					F9,
+					F10 int
+				}{"aaa", 1, 2, 3, 4, 5, 6, 7, 8, 9},
+			},
+			sql: `insert into t1 (F1,F2,F3,F4,F5,F6,F7,F8,F9,F10)` +
+				` values ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10)`,
+			args: []interface{}{"aaa", 1, 2, 3, 4, 5, 6, 7, 8, 9},
+		},
+	}
+
+	run := func(c testCase) {
+		t.Run(c.name, func(t *testing.T) {
+			t.Parallel()
+
+			if c.before != nil {
+				c.before()
+			}
+
+			q, args := BuildInsert(c.opts)
+			defer ResuseArgs(args)
+			if q != c.sql {
+				t.Fatalf("SQL mismatch: `%s` != `%s`", q, c.sql)
+			}
+			if q != c.sql {
+				t.Fatalf("argument list mismatch: `%+v` != `%+v`", args, c.args)
+			}
+
+			if c.after != nil {
+				c.after()
+			}
+		})
+	}
+
+	for i := range cases {
+		run(cases[i])
+	}
+}
